f5250f1e76981fd3b5d2692ceab590818f051f95
DIVIhor <counter088@gmail.com>
2025-06-24 12:15:06 +0000

    feat: Add optional CLI arguments for delay and grid size.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sys import setrecursionlimit
+from sys import setrecursionlimit, argv
 from time import sleep
 
 from graphics import Window
@@ -7,11 +7,37 @@ from maze import Maze
 
 def main():
     setrecursionlimit(5000)
-    win_width = 1080
-    win_height = 1080
-    margin = 50
-    columns = 20
-    rows = 20
+    win_width: int = 1080
+    win_height: int = 1080
+    margin: int = 50
+    columns: int = 20
+    rows: int = 20
+    delay: int = 0
+
+    # user could set whether 0, 1 or 3 arguments
+    # 1 = a delay in seconds before script try to solve the generated maze
+    # 3 = a delay in seconds, a number of columns, and a number of rows
+    arguments: list[str] = argv
+    if len(arguments) >= 2:
+        delay_entry: int = int(arguments[1])
+        # set a 120 seconds limit
+        if 0 < delay_entry < 120:
+            delay = delay_entry
+        elif delay_entry >= 120:
+            delay = 120
+    if len(arguments) == 4:
+        # 50x50 max grid size
+        columns_entry: int = int(arguments[2])
+        if 3 < columns_entry < 50:
+            columns = columns_entry
+        elif columns_entry >= 50:
+            columns = 50
+        rows_entry: int = int(arguments[3])
+        if 3 < rows_entry < 50:
+            rows = rows_entry
+        elif rows_entry >= 50:
+            rows = 50
+
     cell_width = (win_width - 2 * margin) // columns
     cell_height = (win_height - 2 * margin) // rows
 
@@ -19,7 +45,11 @@ def main():
     maze = Maze(margin, margin, rows, columns, cell_width, cell_height, win)
     print("Maze created")
 
-    sleep(10)  # 10s delay to give a chance to solve puzzle
+    # give user time to solve the maze
+    if delay > 0:
+        sleep(delay)
+
+    # solve the maze and print a brief report
     is_solvable = maze.solve()
     if is_solvable:
         print("Maze solved!")
